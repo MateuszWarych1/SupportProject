8c985614dd84c65d14694ca18a38962a1a82cdd5
Mateusz Warych <mateusz_warych@wp.pl>
2024-03-07 20:08:57 +0100

    Aktualizacja logowania (błąd - po rejestracji login oraz hasło wyrzuca na "null" i przenosi do strony error_page, a powinno do logowania. Podobnie się dzieje przy logowaniu, zamiast do zgłoszenia przenosi do error_page

--- src/main/java/com/example/ProjectSupport/Controller/UserController.java
+++ src/main/java/com/example/ProjectSupport/Controller/UserController.java
@@ -1,19 +1,53 @@
 package com.example.ProjectSupport.Controller;
 
+import com.example.ProjectSupport.Service.UserService;
+import com.example.ProjectSupport.model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("loginRequest", new UserModel());
         return "login";
     }
 
     @GetMapping("/registration")
-    public String registration(){
+    public String getRegistration(Model model){
+        model.addAttribute("registerRequest", new UserModel());
         return "registration";
+    }
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute UserModel userModel){
+        System.out.println("register request: " + userModel);
+        UserModel registerUser = userService.registerUser(userModel.getLogin(),
+                userModel.getPassword(), userModel.getEmail());
+        return registerUser == null ? "error_page" : "redirect:/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute UserModel userModel, Model model){
+        System.out.println("login request: " + userModel);
+        UserModel authenticate = userService.authenticate(userModel.getLogin(), userModel.getPassword());
+        if (authenticate != null){
+            model.addAttribute("userLogin", authenticate.getLogin());
+            return "report";
+        }else {
+            return "error_page";
+        }
     }
 
     @GetMapping("/report")
--- src/main/java/com/example/ProjectSupport/model/UserModel.java
+++ src/main/java/com/example/ProjectSupport/model/UserModel.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "user_table")
+@Table(name = "users_table")
 public class UserModel {
 
     @Id
